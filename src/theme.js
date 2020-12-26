91f2d6523f2ae79ba11e0a7305f00d29a718d35b
Valentina Perishko <valentina.perishko@gmail.com>
2020-12-26 10:09:45 -0600

    adds changes

--- src/theme.js
+++ src/theme.js
@@ -13,21 +13,31 @@ const themeInLocalStorage = localStorage.getItem('theme');
 if (themeInLocalStorage === Theme.DARK) {
    refs.switch.checked = true;
    refs.body.classList.add(Theme.DARK);
+} else {
+  refs.body.classList.add(Theme.LIGHT);
 }
+
   
 function changeTheme(e) {
       if (e.target.checked) {
-      refs.body.classList.add(Theme.DARK);
-      refs.body.classList.remove(Theme.LIGHT);
+      // refs.body.classList.add(Theme.DARK);
+      // refs.body.classList.remove(Theme.LIGHT);
+      replaceTheme(Theme.LIGHT, Theme.DARK);
       localStorage.setItem('theme', Theme.DARK);
 
     } else {
-      refs.body.classList.add(Theme.LIGHT);
-      refs.body.classList.remove(Theme.DARK);
-      localStorage.removeItem('theme');
+      // refs.body.classList.add(Theme.LIGHT);
+      // refs.body.classList.remove(Theme.DARK);
+      replaceTheme(Theme.DARK, Theme.LIGHT);
       localStorage.setItem('theme', Theme.LIGHT);
     }
   }
+  const replaceTheme = (oldTheme, newTheme) => {
+    // refs.body.classList.add(Theme.DARK);
+    // refs.body.classList.remove(Theme.LIGHT); 
+    refs.body.classList.toggle(Theme.DARK);
+    refs.body.classList.toggle(Theme.LIGHT);
+    }
 refs.switch.addEventListener('change', changeTheme);
 
 
